a7eb6f1702126a822facce27e9ed42df81404eef
Luca Francesconi <luca.francesconi1977@outlook.com>
2023-10-07 21:48:19 +0200

    Added new example

--- Test.py
+++ Test.py
@@ -1,14 +1,17 @@
 import sys
 
-sys.path.append("c:/Scripts/Utils/")
+sys.path.append("c:/Scripts/MyPythonToolBox/Utils/")
 from fileutil import FileUtil 
 
 f = FileUtil()
-size = f.get_file_size('C:/Scripts/LICENSE')
+size = f.get_file_size('C:/Scripts/MyPythonToolBox/LICENSE')
 print("LICENSE file size (KB): " + str(size))
 
-my_files = f.get_files_in_folder('c:/Scripts')
+last_access = f.get_file_last_access('C:/Scripts/MyPythonToolBox/LICENSE')
+print("LICENSE file last logged on : " + str(last_access))
+
+my_files = f.get_files_in_folder('c:/Scripts/MyPythonToolBox')
 print(my_files)
 
-my_files = f.get_files_in_folder('c:/Scripts','py')
+my_files = f.get_files_in_folder('c:/Scripts/MyPythonToolBox','py')
 print(my_files) 
